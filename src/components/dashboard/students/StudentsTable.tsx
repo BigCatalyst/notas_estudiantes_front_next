75ee60cb1334bda7636e04be74ef23b8397164df
René Lázaro Collado Arteaga <94305086+critBus@users.noreply.github.com>
2025-03-08 12:15:10 -0500

    add filtro por grupo a estudiantes

--- src/components/dashboard/students/StudentsTable.tsx
+++ src/components/dashboard/students/StudentsTable.tsx
@@ -233,6 +233,15 @@ const StudentsTable = () => {
           }
         />
 
+        <input
+          type="text"
+          placeholder="Buscar por Grupo"
+          className="mt-1 p-2 block w-full rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
+          onChange={(e) =>
+            handleFilterChange("group__name__contains", e.target.value)
+          }
+        />
+
         <select
           className="mt-1 p-2 block w-full rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500"
           onChange={(e) => handleFilterChange("sex", e.target.value)}
